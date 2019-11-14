faad6cd37620ab17277d17eaa26e994dcf156024
Yating(Esther) Su <yaringsu@foxmail.com>
2019-11-14 13:04:10 -0500

    Deploying

--- endPoints/index.js
+++ endPoints/index.js
@@ -6,4 +6,8 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../clent/build/index.html"));
+});
+
 module.exports = router;
